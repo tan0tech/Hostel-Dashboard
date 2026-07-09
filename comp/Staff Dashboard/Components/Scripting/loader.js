fe030532b291e7c0a7d222299034fb969300c3d5
tan0tech <sahutanishk25@gmail.com>
2026-07-09 17:56:55 +0530

    Delete comp/Staff Dashboard directory

--- comp/Staff Dashboard/Components/Scripting/loader.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.loader-wrapper').style.display = 'none';
-    }, 4000); // Adjust this time as needed
-});
-
-
